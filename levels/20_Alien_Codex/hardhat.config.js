3738aea9c350860d83c381612d96c3e7af9f1c49
steventhornton <steven@steventhornton.ca>
2022-09-22 08:44:40 -0700

    Added solution to the Alien Codex level

---
+++ levels/20_Alien_Codex/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.5.13",
+  networks: {
+    hardhat: {
+      mining: {
+        auto: false,
+        interval: [3000, 6000]
+      },
+      forking: {
+        url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`
+      },
+      accounts: [{privateKey: process.env.PRIVATE_KEY, balance: '1000000000000000000'}]
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
